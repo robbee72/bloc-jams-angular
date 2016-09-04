2797bb502c3110a2a67f1a130d06def720f87aea
krobnson <krobnson@gmail.com>
2016-09-04 09:15:27 -0700

    service 7 partial commit

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -2,7 +2,6 @@
      function CollectionCtrl(Fixtures) {
              this.albums = Fixtures.getCollection(12);
      }
- 
      angular
          .module('blocJams')
          .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
